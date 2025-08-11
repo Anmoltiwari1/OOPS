d4fd2e0b174d874d0bcc9529778eda728f4a7925
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2025-08-12 00:15:24 +0530

    Continue polymorphism

---
+++ Shapes.java
@@ -0,0 +1,8 @@
+//Polymorphism-->Many ways to represent a entity
+
+
+public class Shapes {
+    void area(){
+        System.out.println("I am in shapes");
+    }
+}
--- Types.java
+++
@@ -1,9 +0,0 @@
-//Single-Inheritance-->When one class inherits fron one ther class
-
-//Multi-level Inheritance-->When one class inherots from above classmand that from one above class and so on.
-
-
-
-public class Types {
-    
-}
